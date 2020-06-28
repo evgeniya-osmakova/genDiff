433e720f5974d865a09d60325daa358a05d3195c
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2020-06-28 22:20:54 +0300

    fix 9

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,45 +2,54 @@ import _ from 'lodash';
 
 const firstTab = '  ';
 
-const makeStylishFormat = (diff) => {
-  const iter = (innerData, treeDepth) => {
-    const stringify = (sign, name, value, depth) => {
-      const tab = `${firstTab.repeat(depth * 2 + 1)}`;
-      if (!_.isObject(value)) {
-        return `${tab}${sign}${name}: ${value}`;
+const makeAST = (data) => {
+  const keys = _.keys(data);
+  return keys.map((key) => {
+    const value = data[key];
+    return (_.isObject(value))
+      ? {
+        name: key,
+        status: 'unchanged',
+        children: makeAST(value),
       }
-      const result = [
-        stringify(sign, name, '{', depth),
-        iter([value].flat(), depth + 1),
-        `${tab}  }`,
-      ];
-      return result.join('\n');
-    };
-
-    const mappingNodeType = {
-      unchanged: ({ name, value }, depth) => stringify('  ', name, value, depth),
-      deleted: ({ name, value }, depth) => stringify('- ', name, value, depth),
-      added: ({ name, value }, depth) => stringify('+ ', name, value, depth),
-      nested: ({ name, children }, depth) => stringify('  ', name, children, depth),
-      changed: (node, depth) => {
-        const { name, addedValue, deletedValue } = node;
-        const strFromAddedValue = mappingNodeType.added({ name, value: addedValue }, depth);
-        const strFromDeletedValue = mappingNodeType.deleted({ name, value: deletedValue }, depth);
-        return [strFromAddedValue, strFromDeletedValue];
-      },
-    };
-
-    const formattedDiff = innerData.flatMap((elem) => {
-      if (!_.has(elem, 'status')) {
-        const name = _.keys(elem)[0];
-        const value = elem[name];
-        return mappingNodeType.unchanged({ name, value }, treeDepth);
-      }
-      return mappingNodeType[elem.status](elem, treeDepth);
-    });
-    return formattedDiff.join('\n');
-  };
-  return ['{', iter(diff, 0), '}'].join('\n');
+      : { name: key, status: 'unchanged', value };
+  });
 };
+
+const stringify = (sign, name, value, depth, status) => {
+  const tab = `${firstTab.repeat(depth * 2 + 1)}`;
+  if (!_.isObject(value)) {
+    return `${tab}${sign}${name}: ${value}`;
+  }
+  const formattedValue = (status === 'nested') ? value : makeAST(value);
+  const result = [
+    stringify(sign, name, '{', depth),
+    // eslint-disable-next-line no-use-before-define
+    iter(formattedValue, depth + 1),
+    `${tab}  }`,
+  ];
+  return result.join('\n');
+};
+
+const mappingNodeType = {
+  unchanged: ({ name, value }, depth) => stringify('  ', name, value, depth),
+  deleted: ({ name, value }, depth) => stringify('- ', name, value, depth),
+  added: ({ name, value }, depth) => stringify('+ ', name, value, depth),
+  nested: ({ name, children, status }, depth) => stringify('  ', name, children, depth, status),
+  changed: (node, depth) => {
+    const { name, addedValue, deletedValue } = node;
+    const strFromAddedValue = mappingNodeType.added({ name, value: addedValue }, depth);
+    const strFromDeletedValue = mappingNodeType.deleted({ name, value: deletedValue }, depth);
+    return [strFromAddedValue, strFromDeletedValue];
+  },
+};
+
+const iter = (innerData, treeDepth) => {
+  const formattedDiff = innerData.flatMap((elem) => mappingNodeType[elem.status](elem,
+    treeDepth, elem.status));
+  return formattedDiff.join('\n');
+};
+
+const makeStylishFormat = (diff) => ['{', iter(diff, 0), '}'].join('\n');
 
 export default makeStylishFormat;
